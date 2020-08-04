a521b15507c789f3a74e2dd58af5df481e796843
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-05 01:06:39 +0530

    Added Background Files

--- api/weatherApi.js
+++ api/weatherApi.js
@@ -1,6 +1,6 @@
 const weatherApi = async (cityName) => {
   const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=ef9b226ebbd53541a328124335d8ad04"
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ef9b226ebbd53541a328124335d8ad04`
   );
   const data = await response.json();
   return data;
--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image, ImageBackground } from "react-native";
 
 const DetailsScreen = ({ route, navigation }) => {
   const { weather } = route.params;
   const icon = weather.weather[0].icon;
-  console.log(icon);
+  console.log(weather);
 
-  navigation.setOptions({ title: weather.name });
+  navigation.setOptions({ headerShown: false });
 
   return (
     <View style={styles.container}>
-      <Text>{weather.name}</Text>
-      <Image
-        source={{
-          uri: `http://openweathermap.org/img/wn/${icon}@4x.png`,
-        }}
-        style={styles.icon}
-      />
-      <Text>Latitude: {weather.coord.lat}</Text>
-      <Text>Longitude: {weather.coord.lon}</Text>
+      <ImageBackground
+        source={require("../assets/backgrounds/drizzle.jpg")}
+        style={styles.background}
+      >
+        <Text>{weather.name}</Text>
+        <Image
+          source={{
+            uri: `http://openweathermap.org/img/wn/${icon}@4x.png`,
+          }}
+          style={styles.icon}
+        />
+        <Text>Weather: {weather.weather[0].main}</Text>
+        <Text>Latitude: {weather.coord.lat}</Text>
+        <Text>Longitude: {weather.coord.lon}</Text>
+        <Text>
+          Temperature: {(weather.main.temp - 273.15).toPrecision(4)} C
+        </Text>
+        <Text>
+          Feels Like: {(weather.main.feels_like - 273.15).toPrecision(4)} C
+        </Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -26,9 +38,14 @@ const DetailsScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  background: {
+    height: "100%",
+    width: "100%",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    opacity: 0.85,
   },
   icon: {
     height: 200,
