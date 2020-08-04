9c8c53221fb7693d293904481ae8bfd7e9ff8259
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-04 22:01:29 +0530

    Added Dynamic Icon to Details Screen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 
 const DetailsScreen = ({ route, navigation }) => {
   const { weather } = route.params;
+  const icon = weather.weather[0].icon;
+  console.log(icon);
 
   navigation.setOptions({ title: weather.name });
 
   return (
     <View style={styles.container}>
       <Text>{weather.name}</Text>
+      <Image
+        source={{
+          uri: `http://openweathermap.org/img/wn/${icon}@4x.png`,
+        }}
+        style={styles.icon}
+      />
       <Text>Latitude: {weather.coord.lat}</Text>
       <Text>Longitude: {weather.coord.lon}</Text>
     </View>
@@ -16,7 +24,16 @@ const DetailsScreen = ({ route, navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  icon: {
+    height: 200,
+    width: 200,
+  },
 });
 
 export default DetailsScreen;
